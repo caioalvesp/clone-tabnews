cbfa086180eb9737a75b046a136831ece1f4ba1d
Caio Alves <78694107+caioalvesp@users.noreply.github.com>
2026-04-08 19:18:21 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,48 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+
+  // QUERIES QUE USEI PARA O EXERCÍCIO
+  // const queryPostgresVersion = await database.query("SHOW server_version;");
+  // const postgresVersion = queryPostgresVersion.rows[0].server_version;
+
+  // const queryMaxConnections = await database.query("SHOW max_connections;");
+  // const dbMaxConnections = queryMaxConnections.rows[0].max_connections;
+
+  // const queryUsedConnections = await database.query(
+  //   "select count (*) from pg_stat_activity WHERE datname = 'local_db';",
+  // );
+  // const usedConnections = queryUsedConnections.rows[0].count;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    chave: "valor",
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,26 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // TESTES QUE FIZ PARA O EXERCÍCIO
+  // const postgresVersion = +responseBody.postgres_version;
+  // expect(postgresVersion).toBeDefined();
+  // expect(postgresVersion).toBeGreaterThan(0);
+
+  // const maxConnections = +responseBody.max_connections;
+  // expect(maxConnections).toBeDefined();
+  // expect(maxConnections).toBeGreaterThan(0);
+
+  // const usedConnections = +responseBody.used_connections;
+  // expect(usedConnections).toBeDefined() ;
+  // expect(usedConnections).toBeGreaterThan(0);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.13");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
